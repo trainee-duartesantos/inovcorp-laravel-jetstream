35a110c64dfda0095fe366b3ab5a646b48846768
Duarte Santos <duarte.n.santos@gmail.com>
2025-11-17 10:45:35 +0000

    feat: implementar sistema de requisições e controle de acesso
    - Criar migrations para roles, role_user, requisições e campo disponível em livros
    - Adicionar models Role e Requisicao com relações
    - Implementar controllers para CRUD de requisições e administração de usuários
    - Criar policies, requests, seeders para controle de acesso
    - Adicionar sistema de emails e notificações para requisições
    - Configurar jobs e commands para lembretes automáticos

---
+++ app/Console/Commands/SendDailyRequisitionR.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SendDailyRequisitionR extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-daily-requisition-r';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
